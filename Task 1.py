e17fa52ba519509adbe74135ab99bd6c34e6712b
dhruvdesai7214 <dhruvdesai1406@gmail.com>
2025-05-21 18:34:41 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+#Task 1: Arithmetic Operations
+#These takes the input Values
+a = input("Enter the First Value:")
+b = input("Enter the Second Value:")
+#After taking both the values these line converts them into integer and transforms the
+print("Here are the Operations",
+      "\nAddition =", int(a) + int(b),
+      "\nSubtraction =", int(a) - int(b),
+      "\nMultiplication =", int(a) * int(b),
+      "\nDivision =", int(a) / int(b))
+
+
+
+
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+#Task 2:Personalized Greetings
+#These lines takes first name last name of users as input
+x=input("Enter your first name:")
+y=input("Enter your Last Name:")
+#These lines converst the input into a personalized Greeting
+print("Hello",x +" "+ y+"!","Welcome to the python program.")
