d0d119207d562440502e0f02be9a418bba6784b9
dhruvdesai7214 <dhruvdesai1406@gmail.com>
2025-05-21 23:38:26 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@
 #These takes the input Values
 a = input("Enter the First Value:")
 b = input("Enter the Second Value:")
-#After taking both the values these line converts them into integer and transforms the
+#After taking both the values these line converts them into integer and does the mathematical operations
 print("Here are the Operations",
       "\nAddition =", int(a) + int(b),
       "\nSubtraction =", int(a) - int(b),
